6204a984490909ef27736d3a90d3536edd81f5b4
MD KAIF SARFRAZ <93869713+sarfrazkaif9@users.noreply.github.com>
2024-09-19 18:41:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+ 
+ "use strict"; // treat all JS code as newer version
+
+ //alert("sum")   // we are using nodejs, not browser
+
+ console.log(3+3)
+ 
+ console.log("kaif")
+
+ let name = "kaif"
+ let age = 24
+ let isLoggidIn = false
+ let state;
+
+// number => 2^53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object =>
+// 
+
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof null);
+console.log(typeof undefined);
