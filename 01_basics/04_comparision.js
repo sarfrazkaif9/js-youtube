dbb6a6ffc3a41a2af9647ae772dd9be1c070406c
MD KAIF SARFRAZ <93869713+sarfrazkaif9@users.noreply.github.com>
2024-09-22 11:57:52 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);  // while trying to compare try to put same data type
+// console.log("02">1); //although js automatically changes string to number to compare the data
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0); //always false
+// console.log(undefined >= 0);
+// console.log(undefined > 0);
+
+console.log("2" == 2 ); 
